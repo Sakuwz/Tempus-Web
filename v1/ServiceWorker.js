bdd71776d77bca64ed29dde6f2f1542ad951bbb1
Sakuna Madushanka <31409220+Sakuwz@users.noreply.github.com>
2025-02-25 18:16:32 +0530

    v2

---
+++ v1/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-Tempus_POC-0.1";
+const contentToCache = [
+    "Build/Tempus-Web.loader.js",
+    "Build/Tempus-Web.framework.js",
+    "Build/Tempus-Web.data",
+    "Build/Tempus-Web.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
